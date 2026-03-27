d67e5b6690e4b2b5b9a7a0eda28ed0381ac12b09
EnzoCatalan <enzo.catalan@kurax.dev>
2026-03-27 15:35:40 -0500

    Refactor imports in BettingSlip and MatchCard test files to use '@testing-library/dom' for screen

--- __tests__/components/BettingSlip.test.tsx
+++ __tests__/components/BettingSlip.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { screen, act } from '@testing-library/react';
+import { screen } from '@testing-library/dom';
+import { act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import BettingSlip from '@/components/schedule/BettingSlip/BettingSlip';
 import { renderWithStore } from '../utils/renderWithStore';
